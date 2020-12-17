e9ab4017804a87da929b0cc685e17c1bc7d302e8
JellyJamm <65462366+JellyJamm@users.noreply.github.com>
2020-12-16 23:17:04 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-name 'ChonkyJellyCopyOutfit'
+name 'CopyFit'
 author 'Chonky Jelly'
-description 'Jelly is Chonky'
+description 'Jelly is Chonky but Chonky Badger is Chonkier'
 version '1.0'
+url 'https://github.com/JellyJamm/copyfit/'
 
 -- Files
 client_script 'client.lua'
