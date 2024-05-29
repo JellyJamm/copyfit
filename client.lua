06a76ae574596f156abe05f9caa4e2e8bf020eeb
6locc 6a6y <65462366+JellyJamm@users.noreply.github.com>
2024-05-28 18:43:57 -0700

    client.lua
    2.0

--- client.lua
+++ client.lua
@@ -1,32 +1,63 @@
 --================================--
---          Copy Fit v1           --
+--          Copy Fit v2           --
 --          by JellyJam           --
 --      License: GNU GPL 3.0      --
 --================================--
 
 function notify(string)
-        SetNotificationTextEntry("STRING")
-        AddTextComponentString(string)
-        DrawNotification(true, false)
-    end
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(string)
+    DrawNotification(true, false)
+end
 
 RegisterNetEvent("getOutfit")
-AddEventHandler("getOutfit", function(playerToGiveOutfit)
-	local ped1 = PlayerPedId()
+AddEventHandler("getOutfit", function(target, diffModels)
+    local ped = PlayerPedId()
+    local model = GetEntityModel(ped)
     local outfit = {}
-for i=1,11 do
-    local drawable, texture, palette = GetPedDrawableVariation(ped1, i), GetPedTextureVariation(ped1, i), GetPedPaletteVariation(ped1, i)
-    table.insert(outfit, {drawable = drawable, texture = texture, palette = palette})
-end -- Get Ped Drawables
-    TriggerServerEvent("sendToServer", outfit, playerToGiveOutfit)
-    notify("~g~ID:"..playerToGiveOutfit.." ~w~copied your outfit")
+    local props = {}
+
+    for i = 1, 11 do
+        local drawable = GetPedDrawableVariation(ped, i)
+        local texture =  GetPedTextureVariation(ped, i)
+        local palette = GetPedPaletteVariation(ped, i)
+        table.insert(outfit, {drawable = drawable, texture = texture, palette = palette})
+    end -- Get Ped Drawables
+    for i = 0, 7 do
+        local index = GetPedPropIndex(ped, i)
+        local textureIndex = GetPedPropTextureIndex(ped, i)
+        table.insert(props, {index = index, textureIndex = textureIndex})
+    end -- Get Ped Props
+
+    TriggerServerEvent("sendToServer", outfit, props, model, target, diffModels)
+    notify("~g~ID:"..target.." ~w~copied your outfit")
 end)
 
 RegisterNetEvent("setPed")
-AddEventHandler("setPed", function(outfit)
-    local ped2 = PlayerPedId()
-    for k,v in pairs(outfit) do
- 		SetPedComponentVariation(ped2, k, v.drawable, v.texture, v.palette)
- 		notify("~b~Outfit successfully copied")
-	end-- Set Ped Components
+AddEventHandler("setPed", function(outfit, props, model, diffModels)
+
+    if diffModels then
+        RequestModel(model)
+        while not HasModelLoaded(model) do
+            Wait(0)
+        end
+
+        SetPlayerModel(PlayerId(), model)
+        SetModelAsNoLongerNeeded(model)
+    end
+
+    local ped = PlayerPedId()
+
+    for i = 1, 11 do
+        SetPedComponentVariation(ped, i, outfit[i].drawable, outfit[i].texture, outfit[i].palette)
+    end-- Set Ped Components
+    for i = 0, 7 do
+        if props[i + 1].index ~= -1 then
+            SetPedPropIndex(ped, i, props[i + 1].index, props[i + 1].textureIndex, false)
+        else
+            ClearPedProp(ped, i)
+        end
+    end -- Set Ped Props
+
+    notify("~b~Outfit successfully copied")
 end)
