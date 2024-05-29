ec4e1142ca4f5de5b153ce23e853af16a8abff12
6locc 6a6y <65462366+JellyJamm@users.noreply.github.com>
2024-05-28 18:42:15 -0700

    server.lua
    2.0

--- server.lua
+++ server.lua
@@ -1,15 +1,19 @@
 --================================--
---          Copy Fit v1           --
+--          Copy Fit v2           --
 --          by JellyJam           --
 --      License: GNU GPL 3.0      --
 --================================--
 
 RegisterCommand("copy", function(source, args, rawCommand)
-    local targetPlayer = tostring(args[1])
-    TriggerClientEvent("getOutfit", targetPlayer, source)
+    local target = tostring(args[1])
+    if GetEntityModel(GetPlayerPed(source)) == GetEntityModel(GetPlayerPed(target)) then
+        TriggerClientEvent("getOutfit", target, source, false)
+    else
+        TriggerClientEvent("getOutfit", target, source, true)
+    end
 end)
 
 RegisterNetEvent("sendToServer")
-AddEventHandler("sendToServer", function(outfit, targetPlayer)
-    TriggerClientEvent("setPed", targetPlayer, outfit)
+AddEventHandler("sendToServer", function(outfit, props, model, target, diffModels)
+    TriggerClientEvent("setPed", target, outfit, props, model, diffModels)
 end)
